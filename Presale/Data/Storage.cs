731fd7b627b9455fa22b06380f21acd5fe75fc14
ivaneverovsky <ivaneverovsky@mail.ru>
2024-03-06 17:21:47 -0800

    lil server + lil client

---
+++ Presale/Data/Storage.cs
@@ -0,0 +1,19 @@
+﻿using Presale.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presale.Data
+{
+    class Storage
+    {
+        private List<User> _users = new List<User>();
+        public List<User> Users { get { return _users; } }
+        public void AddUser(User user)
+        {
+            _users.Add(user);
+        }
+    }
+}
---
+++ Presale/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presale.Models
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string AccountType { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public User(int id, string name, string surname, string email, string phone, string accountType, string login, string password)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Phone = phone;
+            AccountType = accountType;
+            Login = login;
+            Password = password;
+
+        }
+    }
+}
